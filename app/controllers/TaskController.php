eb498b88d43af21c1133294cad7a098b5e432d5b
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-05-14 10:50:42 +0200

    indexAction & poblar tasks

---
+++ app/controllers/TaskController.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once ROOT_PATH . '/app/models/TaskModel.class.php';
+
+class TaskController extends Controller
+{
+
+
+  public function __construct(private $taskModel = new Task)
+  {
+  }
+
+  public function indexAction()
+  {
+    $allTasks = $this->taskModel->fetchAll();
+    $this->view->allTasks = $allTasks;
+  }
+}
