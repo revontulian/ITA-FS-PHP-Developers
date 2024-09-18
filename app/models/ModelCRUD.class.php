0fe84e6854398de0a698ea2bd2c21380f7435c92
Jonathan Caballero <jonathancaballero916@gmail.com>
2024-09-18 13:24:14 +0200

    creacion del conructor de la clase modelo

--- app/models/ModelCRUD.class.php
+++ app/models/ModelCRUD.class.php
@@ -1,6 +1,10 @@
 <?php
 
 class ModelCRUD extends Model {
+    protected array $alltask;
+    protected string $dbRoute = ROOT_PATH . '\app\dataBase\data.json'; 
     
-    
+    public function __construct() {
+        $this->loadtask();
+    }
 }
