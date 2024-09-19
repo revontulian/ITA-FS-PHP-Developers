90917eb04303ab92f8aa22a804d4f5d328944e1b
Jonathan Caballero <jonathancaballero916@gmail.com>
2024-09-19 10:46:01 +0200

    funcion create en el modelo

--- app/models/ModelCRUD.class.php
+++ app/models/ModelCRUD.class.php
@@ -10,4 +10,17 @@ class ModelCRUD extends Model {
     public function loadTask(){
         $this->alltask = json_decode(file_get_contents($dbRoute), true) ?? [];
     }
+    public function create(array $data){
+        $id = count($this->alltask)+ 1;
+        $task = [
+            'id' => $id,
+            'title' => $data['title'],
+            'status' => 'pending',
+            'startTime' => $data['starTime'],
+            'deadLine' => $data['deadLine'],
+        ];
+        $this->allTask[] = $task;
+        file_put_contents($this->dataRoute, json_encode($this->allTasks, JSON_PRETTY_PRINT));
+    }
+    
 }
