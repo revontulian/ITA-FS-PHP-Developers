07abd015269407e4752c419ebcf48b4c25c4ca44
Jonathan Caballero <jonathancaballero916@gmail.com>
2024-09-22 19:10:57 +0200

    controller construct y cambio de view

--- app/controllers/TaskController.php
+++ app/controllers/TaskController.php
@@ -1,5 +1,12 @@
 <?php
+require_once ROOT_PATH. '/app/models/ModelCRUD.class.php';
+
 class TaskController extends Controller{
+   protected $modelCrud;
+
+   public function __construct() {
+    $this->$modelCrud = new Task();
+   }
     public function indexAction (){
         echo 'Bicha pajua jajajaja';
     }
